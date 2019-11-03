bf505b35a52b5dfca50a6ea614124d9f4b7d141d
joonardo <joonatan.bergholm@taitounited.fi>
2019-11-03 15:52:48 +0200

    feat: page change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+import configureStore from "./root.saga";
+import { Page } from "./Page";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Page />
     </div>
-  );
-}
+  </Provider>
+);
 
 export default App;
