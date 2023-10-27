5e1e1e907b7f1a9b99b7d848943e0fcb244f6812
Aapo Laakkio <47868046+ApsiV11@users.noreply.github.com>
2023-10-27 14:24:20 +0300

    Update reload_infonaytto.sh

--- reload_infonaytto.sh
+++ reload_infonaytto.sh
@@ -27,11 +27,7 @@ if [[ "$@" == *"--restart"* ]]
 then
 
 	#NOTE: killing chromium makes it think it has crashed, to prevent it from nagging, go to about:config and set toolkit.startup.max_resumed_crashes to -1
- 	set -e
-	tmux kill-session -t page
-	tmux kill-session -t bot
-	killall chromium-browser
- 	set +e
+ 	sh shutdown.sh
 
 	DISPLAY=:0 sh launch_infonaytto.sh
 
