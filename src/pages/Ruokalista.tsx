694196686af704f06b4932b55bc82ed4c03c6758
Aapo Laakkio <aapolaakkio@outlook.com>
2024-11-29 14:22:15 +0200

    Hopefully fix Ruokalista

--- src/pages/Ruokalista.tsx
+++ src/pages/Ruokalista.tsx
@@ -18,11 +18,6 @@ type RestaurantMenu = [
   Record<string, { title: string; properties: string[] }[]>
 ]
 
-// TODO: Kämälista that respects capitalization
-const date = new Date()
-const today = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-  .toISOString()
-  .substring(0, 10)
 const timePerFrame = 5
 
 // Remove item from Fazer menus to make them fit
@@ -35,15 +30,19 @@ const pruneDish = (str: string) => {
   )
 }
 
-const listDishes = (menu: RestaurantMenu, date: string) => {
+const listDishes = (menu: RestaurantMenu) => {
+  const date = new Date()
+  const today = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    .toISOString()
+    .substring(0, 10)
   if (!menu) {
     return <p>Ei aukiolevia ravintoloita</p>
   }
 
-  if (menu[1]?.[date]) {
+  if (menu[1]?.[today]) {
     return (
       <div>
-        {menu[1]?.[date].map((dish, index) =>
+        {menu[1]?.[today].map((dish, index) =>
           pruneDish(dish.title) ? (
             ''
           ) : (
@@ -61,11 +60,11 @@ const listDishes = (menu: RestaurantMenu, date: string) => {
   }
 }
 
-const Menu = ({ menu, date }: { menu: RestaurantMenu; date: string }) => {
+const Menu = ({ menu }: { menu: RestaurantMenu }) => {
   return (
     <div className={styles.mask}>
       {menu && <h1 className={styles.h1}> {restaurants[menu[0]]} </h1>}
-      <div className={styles.text}>{listDishes(menu, date)}</div>
+      <div className={styles.text}>{listDishes(menu)}</div>
     </div>
   )
 }
@@ -93,7 +92,7 @@ export const Ruokalista = ({ showNext }: PageProps) => {
 
   return (
     <div className={styles.background}>
-      <Menu menu={menu?.[indexOfRestaurantID]} date={today} />
+      <Menu menu={menu?.[indexOfRestaurantID]} />
     </div>
   )
 }
